938cbbccfdc9e846accffde5020b87a69e332174
Mikael <mika.sjj@hotmail.com>
2025-10-08 17:32:13 -0300

    refactor: class without lombok

--- src/main/java/br/com/bradesco/codebankers/scam_api/domain/blacklist/BlacklistItem.java
+++ src/main/java/br/com/bradesco/codebankers/scam_api/domain/blacklist/BlacklistItem.java
@@ -9,16 +9,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Entity(name = "BlacklistItem")
 @Table(name = "blacklist_items")
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class BlacklistItem {
 
@@ -35,4 +29,28 @@ public class BlacklistItem {
 
     private String source; //denuncia de cliente; lista extern xpto
 
+    public BlacklistItem(){}
+
+    public BlacklistItem(Long id, ItemType itemType, String itemValue, String source) {
+        this.id = id;
+        this.itemType = itemType;
+        this.itemValue = itemValue;
+        this.source = source;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public ItemType getItemType() {
+        return itemType;
+    }
+
+    public String getItemValue() {
+        return itemValue;
+    }
+
+    public String getSource() {
+        return source;
+    }
 }
