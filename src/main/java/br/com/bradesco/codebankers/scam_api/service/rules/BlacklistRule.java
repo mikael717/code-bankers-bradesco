e7963ebd4555b337d20a8d18bb5e70764e6ff5a5
Mikael <mika.sjj@hotmail.com>
2025-10-25 16:39:35 -0300

    refactor: changes in the verify algorithm including normalization

--- src/main/java/br/com/bradesco/codebankers/scam_api/service/rules/BlacklistRule.java
+++ src/main/java/br/com/bradesco/codebankers/scam_api/service/rules/BlacklistRule.java
@@ -1,7 +1,9 @@
 package br.com.bradesco.codebankers.scam_api.service.rules;
 
+import br.com.bradesco.codebankers.scam_api.domain.ItemType;
 import br.com.bradesco.codebankers.scam_api.domain.blacklist.BlacklistRepository;
 import br.com.bradesco.codebankers.scam_api.dto.VerificationRequest;
+import br.com.bradesco.codebankers.scam_api.util.NormalizerUtil;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -17,9 +19,25 @@ public class BlacklistRule implements VerificationRule{
 
     @Override
     public List<String> verify(VerificationRequest request) {
-        if(blacklistRepository.existsByItemValue(request.itemValue())){
-            return List.of("ITEM_FOUND_IN_BLACKLIST");
+
+        final String rawValue = request.itemValue();
+        final ItemType itemType = request.itemType();
+
+        final String normalizedValue = switch (itemType){
+            case EMAIL -> NormalizerUtil.normalizeEmail(rawValue);
+            case PHONE -> NormalizerUtil.normalizePhone(rawValue);
+            case URL -> NormalizerUtil.normalizeUrl(rawValue);
+        };
+
+        boolean isBlacklisted = blacklistRepository.existsByItemTypeAndItemValue(itemType, normalizedValue);
+
+        if(!isBlacklisted){
+            isBlacklisted = blacklistRepository.existsByItemTypeAndItemValue(itemType, rawValue);
         }
+
+        if(isBlacklisted) return List.of("ITEM_FOUND_IN_BLACKLIST");
+
+
         return Collections.emptyList();
     }
 }
--- src/main/java/br/com/bradesco/codebankers/scam_api/service/rules/WhitelistRule.java
+++ src/main/java/br/com/bradesco/codebankers/scam_api/service/rules/WhitelistRule.java
@@ -1,8 +1,9 @@
 package br.com.bradesco.codebankers.scam_api.service.rules;
 
+import br.com.bradesco.codebankers.scam_api.domain.ItemType;
 import br.com.bradesco.codebankers.scam_api.domain.whitelist.WhitelistRepository;
 import br.com.bradesco.codebankers.scam_api.dto.VerificationRequest;
-import lombok.AllArgsConstructor;
+import br.com.bradesco.codebankers.scam_api.util.NormalizerUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,11 +15,30 @@ public class WhitelistRule implements VerificationRule{
 
     @Autowired
     private WhitelistRepository whitelistRepository;
+
     @Override
     public List<String> verify(VerificationRequest request) {
-        if(whitelistRepository.existsByItemValue(request.itemValue())){
-            return List.of("ITEM_FOUND_IN_WHITELIST");
+
+        //1.normaliza conforme o tipo
+        final String rawValue = request.itemValue();
+        final ItemType itemType = request.itemType();
+
+        final String normalizedValue = switch (itemType) {
+            case EMAIL -> NormalizerUtil.normalizeEmail(rawValue);
+            case PHONE -> NormalizerUtil.normalizePhone(rawValue);
+            case URL ->  NormalizerUtil.normalizeUrl(rawValue);
+        };
+
+        //2.valida primeiro o valor normalizado
+        boolean isWhitelisted = whitelistRepository.existsByItemTypeAndItemValue(itemType, normalizedValue);
+
+        //3.tbm valida o valor bruto(dados legados)
+        if(!isWhitelisted){
+            isWhitelisted = whitelistRepository.existsByItemTypeAndItemValue(itemType, rawValue);
         }
+
+        if(isWhitelisted) return List.of("ITEM_FOUND_IN_WHITELIST");
+
         return Collections.emptyList();
     }
 }
